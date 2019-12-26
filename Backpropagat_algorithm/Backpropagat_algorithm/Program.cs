772c4dda0de79d616f5a7960f5334ecf650017dc
Nicolas191929 <krezu191996@gmail.com>
2019-12-27 00:46:55 +0200

    P

---
+++ Backpropagat_algorithm/Backpropagat_algorithm/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace BackPropagationXor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            train();
+        }
+
+        class sigmoid
+        {
+            public static double output(double x)
+            {
+                return 1.0 / (1.0 + Math.Exp(-x));
+            }
+
+            public static double derivative(double x)
+            {
+                return x * (1 - x);
+            }
+        }
+
+        class Neuron
+        {
+            public double[] inputs = new double[2];
+            public double[] weights = new double[2];
+            public double error;
+
+            private double biasWeight;
+
+            private Random r = new Random();
+
+            public double output
+            {
+                get { return sigmoid.output(weights[0] * inputs[0] + weights[1] * inputs[1] + biasWeight); }
+            }
+
+            public void randomizeWeights()
+            {
+                weights[0] = r.NextDouble();
+                weights[1] = r.NextDouble();
+                biasWeight = r.NextDouble();
+            }
+
+            public void adjustWeights()
+            {
+                weights[0] += error * inputs[0];
+                weights[1] += error * inputs[1];
+                biasWeight += error;
+            }
+        }
+
+        private static void train()
+        {
+            // вхідні значення
+            double[,] inputs =
+            {
+                { 0, 0},
+                { 0, 1},
+                { 1, 0},
+                { 1, 1}
+            };
+
+            //  бажані результати
+            double[] results = { 0, 1, 1, 0 };
+
+            // створення нейронів
+            Neuron hiddenNeuron1 = new Neuron();
+            Neuron hiddenNeuron2 = new Neuron();
+            Neuron outputNeuron = new Neuron();
+
+            // рандомні ваги
+            hiddenNeuron1.randomizeWeights();
+            hiddenNeuron2.randomizeWeights();
+            outputNeuron.randomizeWeights();
+
+            int epoch = 0;
+
+        Retry:
+            epoch++;
+            for (int i = 0; i < 4; i++)  // Дуже важливо, Не тренуватись тише за одним прикладом
+            {
+                // 1) пряме проходження (обилсюємо вихід нейронної мережі)
+                hiddenNeuron1.inputs = new double[] { inputs[i, 0], inputs[i, 1] };
+                hiddenNeuron2.inputs = new double[] { inputs[i, 0], inputs[i, 1] };
+
+                outputNeuron.inputs = new double[] { hiddenNeuron1.output, hiddenNeuron2.output };
+
+                Console.WriteLine("{0} xor {1} = {2}", inputs[i, 0], inputs[i, 1], outputNeuron.output);
+
+                // 2) зворотнє проходження (регулювання вагів)
+
+                // регулює вагу вихідного нейрона, виходячи з його похибки
+                outputNeuron.error = sigmoid.derivative(outputNeuron.output) * (results[i] - outputNeuron.output);
+                outputNeuron.adjustWeights();
+
+                // потім коригує ваги прихованих нейронів, виходячи з його помилок
+                hiddenNeuron1.error = sigmoid.derivative(hiddenNeuron1.output) * outputNeuron.error * outputNeuron.weights[0];
+                hiddenNeuron2.error = sigmoid.derivative(hiddenNeuron2.output) * outputNeuron.error * outputNeuron.weights[1];
+
+                hiddenNeuron1.adjustWeights();
+                hiddenNeuron2.adjustWeights();
+            }
+
+            if (epoch < 2000)
+                goto Retry;
+
+            Console.ReadLine();
+        }
+    }
+}
